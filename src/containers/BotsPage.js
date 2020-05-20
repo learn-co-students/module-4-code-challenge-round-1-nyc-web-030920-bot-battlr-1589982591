209ce1352a5ca725e2dabfd1332f27b3372a1f61
komald4 <komald.4@gmail.com>
2020-05-20 10:28:33 -0400

    1st ccommit

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 
 class BotsPage extends Component {
   //start here with your code for step one
+  state = {
+    bots: []  //creates an empty array for fetch API
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:6001/bots")
+    .then(response => response.json())
+    .then(bots => this.setState({bots: bots}))
+    console.log(this.state.bots)
+  }
+
 
   render() {
-    return <div>{/* put your components here */}</div>;
-  }
+    return (  <div>
+      <BotCollection bots={this.state.bots} />
+    </div>
+  )
+    }
 }
 
 export default BotsPage;
